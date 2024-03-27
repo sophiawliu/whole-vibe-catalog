d1a9b42ae05db39f99421227816b61ada60f2f29
Sophia Liu <sophiawliu@berkeley.edu>
2024-03-26 22:10:30 -0700

    fixed

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,6 +4,7 @@ import LogIn from './LogIn';
 import FunctionPurpose from './FunctionPurpose';
 import * as ReactDOM from 'react-dom';
 import Index from './Index';
+import Popup from 'reactjs-popup';
 
 export function handleLogOut() {
     removeElementByClass("Cover");
@@ -43,46 +44,95 @@ function closeEdit() {
 
 function Edit() {
     return (
-    <div className="Edit">
-        <div className='index-page-center'>
-            <form className='upload-form'>
-                <h1 className='upload-new-cool'>Upload New Cool</h1>
-                {/* <div className='x-button' onClick={closeEdit}>
-                    <div className='x'>x</div>
-                </div> */}
-                <div className='inputs'>
-                    <form className='upload-new-cool'>
-                        <div className='input-section'>
-                            <label className='upload-new-label' for="upload-image">UPLOAD IMAGE</label>
-                            <input className='upload-new-input' id='upload-image' name='upload-image' type='file'></input>
+
+        <Popup trigger=
+        {<div className='edit-black-index' title="Upload new cool">
+            <div className='plus'>+</div>
+        </div>} 
+        modal nested>
+        {
+            close => (
+                <div className='modal'>
+                    <div className="Edit">
+                                <form className='upload-form'>
+                                    <h1 className='upload-new-cool'>Upload New Cool</h1>
+                                    <div className='inputs'>
+                                        <form className='upload-new-cool'>
+                                            <div className='input-section'>
+                                                <label className='upload-new-label' for="upload-image">UPLOAD IMAGE</label>
+                                                <input className='upload-new-input' id='upload-image' name='upload-image' type='file'></input>
+                                            </div>
+                                            <div className='input-section'>
+                                                <label className='upload-new-label' for="vibe-dropdown">VIBE</label>
+                                                <select id='vibe-dropdown' name='vibe-dropdown'>
+                                                    <option value='eighties'>'80s On The Water</option>
+                                                    <option value='crybaby'>Crybaby Greaser</option>
+                                                    <option value='womyns'>Womyn's Folk</option>
+                                                    <option value='austin'>Austin Powers x '60s Mod</option>
+                                                    <option value='pc'>PC Revolution</option>
+                                                    <option value='cowboy'>Cowboy Western</option>
+                                                </select>
+                                            </div>
+                                            <div className='input-section'>
+                                                <label className='upload-new-label' for="cool-description">DESCRIPTION</label>
+                                                <textarea className='upload-new-input' id='cool-description' name='cool-description' rows={3}></textarea>
+                                            </div>
+                                            <div className='input-section'>
+                                                <label className='upload-new-label' for="cool-description">TAGS</label>
+                                                {/* tagify later */}
+                                                <input className='upload-new-input' name='tags'></input>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <button className='upload-button' type='submit'>UPLOAD</button>
+                                    <div className='close' onClick={() => close()}>Close</div>
+                                </form>
                         </div>
-                        <div className='input-section'>
-                            <label className='upload-new-label' for="vibe-dropdown">VIBE</label>
-                            <select id='vibe-dropdown' name='vibe-dropdown'>
-                                <option value='eighties'>'80s On The Water</option>
-                                <option value='crybaby'>Crybaby Greaser</option>
-                                <option value='womyns'>Womyn's Folk</option>
-                                <option value='austin'>Austin Powers x '60s Mod</option>
-                                <option value='pc'>PC Revolution</option>
-                                <option value='cowboy'>Cowboy Western</option>
-                            </select>
-                        </div>
-                        <div className='input-section'>
-                            <label className='upload-new-label' for="cool-description">DESCRIPTION</label>
-                            <textarea className='upload-new-input' id='cool-description' name='cool-description' rows={3}></textarea>
-                        </div>
-                        <div className='input-section'>
-                            <label className='upload-new-label' for="cool-description">TAGS</label>
-                            {/* tagify later */}
-                            <input className='upload-new-input' name='tags'></input>
-                        </div>
-                    </form>
                 </div>
-                <button className='upload-button' type='submit'>UPLOAD</button>
-                <div className='close' onClick={closeEdit}>Close</div>
-            </form>
-        </div>
-    </div>
+            )
+        }
+    </Popup>
+
+    // <div className="Edit">
+    //     <div className='index-page-center'>
+    //         <form className='upload-form'>
+    //             <h1 className='upload-new-cool'>Upload New Cool</h1>
+    //             {/* <div className='x-button' onClick={closeEdit}>
+    //                 <div className='x'>x</div>
+    //             </div> */}
+    //             <div className='inputs'>
+    //                 <form className='upload-new-cool'>
+    //                     <div className='input-section'>
+    //                         <label className='upload-new-label' for="upload-image">UPLOAD IMAGE</label>
+    //                         <input className='upload-new-input' id='upload-image' name='upload-image' type='file'></input>
+    //                     </div>
+    //                     <div className='input-section'>
+    //                         <label className='upload-new-label' for="vibe-dropdown">VIBE</label>
+    //                         <select id='vibe-dropdown' name='vibe-dropdown'>
+    //                             <option value='eighties'>'80s On The Water</option>
+    //                             <option value='crybaby'>Crybaby Greaser</option>
+    //                             <option value='womyns'>Womyn's Folk</option>
+    //                             <option value='austin'>Austin Powers x '60s Mod</option>
+    //                             <option value='pc'>PC Revolution</option>
+    //                             <option value='cowboy'>Cowboy Western</option>
+    //                         </select>
+    //                     </div>
+    //                     <div className='input-section'>
+    //                         <label className='upload-new-label' for="cool-description">DESCRIPTION</label>
+    //                         <textarea className='upload-new-input' id='cool-description' name='cool-description' rows={3}></textarea>
+    //                     </div>
+    //                     <div className='input-section'>
+    //                         <label className='upload-new-label' for="cool-description">TAGS</label>
+    //                         {/* tagify later */}
+    //                         <input className='upload-new-input' name='tags'></input>
+    //                     </div>
+    //                 </form>
+    //             </div>
+    //             <button className='upload-button' type='submit'>UPLOAD</button>
+    //             <div className='close' onClick={closeEdit}>Close</div>
+    //         </form>
+    //     </div>
+    // </div>
   );
 }
 
