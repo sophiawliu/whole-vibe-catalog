c27b5093ce8595f0d7943167601281793b4336fb
Sophia Liu <sophiawliu@berkeley.edu>
2024-03-29 16:53:41 -0700

    uh

--- src/components/index/VibeCard.js
+++ src/components/index/VibeCard.js
@@ -3,7 +3,7 @@ import '../Index.css';
 export default function VibeCard() {
     return (
         <div className='vibe-wrapper'>
-            <img src='../../images/hiroshi.png'></img>
+            <img src='../../images/hiroshi.png' alt='vibe'></img>
             <div className='vibe-title'>'80s On The Water</div>
             <div className='vibe-description'>POOLSUITE FM & VACATION INC</div>
         </div>
