8a02c4a29d822e0b95c93ce78dc576b50c25cb88
Sophia Liu <sophiawliu@berkeley.edu>
2024-03-31 20:23:15 -0700

    hookt!

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,8 +4,44 @@ import 'reactjs-popup/dist/index.css';
 import TopMenu from './menu/TopMenu';
 import BottomMenu from './menu/BottomMenu';
 import VibeCard from './index/VibeCard';
+import { useEffect, useState } from 'react';
+import {
+    collection,
+    getDocs,
+    deleteDoc,
+    doc,
+    onSnapshot,
+  } from "firebase/firestore";
+import {db} from '../firebase';
 
 function Index() {
+    const [data, setData] = useState([]);
+    const userID = localStorage.getItem('uid');
+    
+    useEffect(() => {
+        const unsub = onSnapshot(
+            collection(db, "vibes"),
+            (snapShot) => {
+              let list = [];
+              snapShot.docs.forEach((doc) => {
+                  const uid = doc._document.data.value.mapValue.fields.uid.stringValue;
+                  if (uid === userID) {
+                    list.push({ id: doc.id, ...doc.data() });
+                  }
+              });
+              setData(list);
+              console.log(list);
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+      
+          return () => {
+            unsub();
+          };
+        }, []);
+
     return (
         <div className="Index">
             <div className='top-menu-container'>
@@ -14,11 +50,9 @@ function Index() {
             <div className='index-page'>
                 <h1 className='page-title'>TABLE OF VIBES</h1>
                 <div className='vibe-cards'>
-                    <VibeCard></VibeCard>
-                    <VibeCard></VibeCard>
-                    <VibeCard></VibeCard>
-                    <VibeCard></VibeCard>
-                    <VibeCard></VibeCard>
+                    {data.map((vibe) => (
+                        <VibeCard data={vibe}></VibeCard>
+                    ))}
                 </div>
             </div>
             <div className='bottom-menu-container'>
--- src/components/index/VibeCard.js
+++ src/components/index/VibeCard.js
@@ -2,15 +2,15 @@ import './VibeCard.css';
 
 export default function VibeCard({ data }) {
     return (
-        <div className='card-wrapper'>
-            <img className='card-image' src='hiroshi.png' alt="'80s On The Water"></img>
-            <div className='card-title'>'80s On The Water</div>
-            <div className='card-description'>Poolsuite FM & Vacation Inc.</div>
-        </div>
         // <div className='card-wrapper'>
-        //     <img className='card-image' src={data.img} alt={data.vibeTitle}></img>
-        //     <div className='card-title'>{data.vibeTitle}</div>
-        //     <div className='card-description'>{data.vibeDescription}</div>
+        //     <img className='card-image' src='hiroshi.png' alt="'80s On The Water"></img>
+        //     <div className='card-title'>'80s On The Water</div>
+        //     <div className='card-description'>Poolsuite FM & Vacation Inc.</div>
         // </div>
+        <div className='card-wrapper'>{console.log('HERE', data.img)}
+            {data.img ? <img className='card-image' src={data.img} alt={data.vibeTitle}></img> : <img className='card-image' src={'default-cover.png'} alt={data.vibeTitle}></img>}
+            <div className='card-title'>{data.vibeTitle}</div>
+            <div className='card-description'>{data.vibeDescription}</div>
+        </div>
     )
 }
